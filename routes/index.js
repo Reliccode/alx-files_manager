da47421e3523169f082baf043d4d6f28131e5b5d
Reliccode <bryanmaritim@gmail.com>
2024-03-10 12:00:25 +0300

    adding router for file control

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 // Create router
 const router = express.Router();
@@ -21,5 +22,8 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+
+// Endpoint for uploading files
+router.post('/files', FilesController.postUpload);
 
 export default router;
