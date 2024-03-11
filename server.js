1ffea08b11c535fe7e9ef8da897806810819ba31
Reliccode <bryanmaritim@gmail.com>
2024-03-11 14:30:00 +0300

    express server

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 import express from 'express';
-import routes from './routes/index';
+import controllerRouting from './routes/index';
 
-// Create Express app
 const app = express();
+const port = process.env.PORT || 5000;
 
-// Set port from enviroment variable or default to 5000
-const PORT = process.env.PORT || 5000;
-
-//  Middleware to parse JSON request bodies
 app.use(express.json());
 
-// Load all routes from routes/index.js
-app.use('/', routes);
+controllerRouting(app);
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
+export default app;
